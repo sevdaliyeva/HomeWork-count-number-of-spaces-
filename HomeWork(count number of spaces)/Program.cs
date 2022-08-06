f8c47cfbf12bfc0a8220fed893fe20862f450f3f
Reboot <sevdaia@code.edu.az>
2022-08-07 01:29:26 +0300

    Add project files.

---
+++ HomeWork(count number of spaces)/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int count = 0, index = 0, lastIndex = 0;
+string s = "    this is a string";
+
+index = s.IndexOf(" ");
+while (index > -1)
+{
+    count++;
+    index = s.IndexOf(" ", index + 1);
+
+    if ((index - lastIndex) > 1)
+        break;
+
+    lastIndex = index;
+}
+
+Console.WriteLine(count);
+
